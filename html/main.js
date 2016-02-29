d9787b8967705ab6921b0408b72aae6d192fa986
CJ Ting <fatelovely1128@gmail.com>
2016-02-29 22:26:42 +0800

    Basic jQuery version

--- html/main.js
+++ html/main.js
@@ -2,7 +2,6 @@ var $username = $("[name=username]")
 var $password = $("[name=password]")
 var $btn = $("button")
 
-
 $btn.click(function(evt) {
   evt.preventDefault()
 
@@ -18,3 +17,53 @@ $btn.click(function(evt) {
     },
   })
 })
+
+// Status variable
+var usernameAvailable, checkingUsername
+
+$username.keyup(function(evt) {
+  usernameAvailable = false
+  checkingUsername = true
+  setButtonState()
+  setIndicator()
+  $.ajax({
+    url: "/check",
+    data: {
+      username: $username.val(),
+    },
+    success: function(res) {
+      checkingUsername = false
+      usernameAvailable = res.available
+      setButtonState()
+      setIndicator()
+      showResult(res.available)
+    },
+  })
+})
+
+$password.keyup(function(evt) {
+  setButtonState()
+})
+
+function setButtonState() {
+  var enabled = $username.val().length > 0 &&
+    $password.val().length > 0 &&
+    usernameAvailable
+  $btn.prop("disabled", !enabled)
+}
+
+function setIndicator() {
+  $("#result-ok").hide()
+  $("#result-bad").hide()
+  if(checkingUsername) {
+    $("#indicator").show()
+  } else {
+    $("#indicator").hide()
+  }
+}
+
+function showResult(available) {
+  available ? $("#result-ok").show() : $("#result-bad").show()
+}
+
+setButtonState()
